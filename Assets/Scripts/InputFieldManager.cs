9b9ebebbc796c5895f8fc860f3aa04fb3d930231
Ryan Feldman <ryanjfeldman@gmail.com>
2017-06-18 11:22:36 -0400

    Forgot to add all my files! Oops. - Ryan

---
+++ Assets/Scripts/InputFieldManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldManager : MonoBehaviour {
+
+	public Text consoleTextBox;
+	public InputField commandLine;
+
+	// Use this for initialization
+	void Start () {
+		commandLine.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		commandLine.ActivateInputField();
+	}
+
+	// Sends a string to the console
+	public void SendToConsole() {
+		if (Input.GetButtonDown ("Submit")) {
+			print ("SendToConsole: " + commandLine.text);
+			string currText = consoleTextBox.text;
+			consoleTextBox.text = currText + "\n" + commandLine.text;
+			commandLine.text = "";
+		}
+
+	}
+}
---
+++ Assets/Scripts/RelayMessage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RelayMessage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//point the text child object of the input field here
+	public Text inputText;
+	string messageToSend;
+
+	public void Send()
+	{
+		if(Input.GetButtonDown("Submit")){
+			messageToSend = inputText.text;
+		}
+	}
+}
