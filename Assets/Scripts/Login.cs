7ab9ed4bdc2363163c5f00a25472eaeeebefaa8d
Ryan Feldman <ryanjfeldman@gmail.com>
2017-06-21 09:56:29 -0400

    Changes made to get stuff working on my end with Tammi's new script for Github photos. Looks great. - Ryan

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour {
+
+	private string username = "";
+	private string password = "";
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+	public InputField commandLine;
+	public string sceneName = "";
+
+	// Use this for initialization
+	void Start () {
+		commandLine.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		commandLine.ActivateInputField();
+	}
+
+	public void UpdateConsole() {
+		if (Input.GetButtonDown ("Submit")) {
+			commandLine.text = "";
+			SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+		}
+	}
+}
