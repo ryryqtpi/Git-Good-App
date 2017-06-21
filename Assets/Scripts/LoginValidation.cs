5f0a65fa9f29b3b836ee95eea2de01bd2f98867f
tammiet <tammie.thong@gmail.com>
2017-06-21 09:23:08 -0400

    parsing user information and displaying profile photo enabled

--- Assets/Scripts/LoginValidation.cs
+++ Assets/Scripts/LoginValidation.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 
 public class LoginValidation : MonoBehaviour {
 
-    public string token = "b3601abb8df2e1b2f128c03c148e221dfb89e1cf";
+    //public string token = "b3601abb8df2e1b2f128c03c148e221dfb89e1cf";
     public string username = "tammiet";
+    public string avatar_url;
+    public Image myImage;
 
     // Use this for initialization
     void Start () {
         string url_attribute = "https://api.github.com/users/" + username;
         StartCoroutine(GetJSON(url_attribute));
-
+   
     }
 
     IEnumerator GetJSON(string url)
@@ -27,6 +30,26 @@ public class LoginValidation : MonoBehaviour {
         else
         {
             Debug.Log(www.downloadHandler.text);
+            JsonUtility.FromJsonOverwrite(www.downloadHandler.text, this);
+            string image_url = (this.avatar_url);
+            var www_image = new WWW(image_url);
+            yield return www_image;
+            
+            //if (Application.internetReachability == NetworkReachability.NotReachable)
+            //{
+            //    yield return null;
+            //}
+            Texture2D texture = new Texture2D(1, 1);
+            www_image.LoadImageIntoTexture(texture);
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one / 2);
+            GetComponent<SpriteRenderer>().sprite = sprite;
+
         }
     }
+    // void setUserInfo(string json)
+    //{
+    //    JsonUtility.FromJsonOverwrite(json, this);
+    //    print (this.avatar_url);
+    //}
+    
 }
