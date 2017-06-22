af72cac49612a0ff041189cbeda780968f6e9cd0
tammiet <tammie.thong@gmail.com>
2017-06-22 17:11:27 -0400

    parsed json data into User class

--- Assets/Scripts/LoginManager.cs
+++ Assets/Scripts/LoginManager.cs
@@ -5,9 +5,9 @@ using UnityEngine.Networking;
 
 public class LoginManager : MonoBehaviour {
 
-	//public string token = "b3601abb8df2e1b2f128c03c148e221dfb89e1cf";
-	//public string username = "tammiet";
-	public string username = "";
+    //public string token = "7047406dd90878ac3e665dcc6b7e4771c4809473";
+    //public string username = "tammiet";
+    public string username = "";
 	public string token = "";
 	public string avatar_url;
 	public string total_private_repos;
@@ -55,7 +55,7 @@ public class LoginManager : MonoBehaviour {
 
 	public void AuthUser(string un, string tk)
 	{
-		string url_attribute = "https://api.github.com/users/" + un;
+		string url_attribute = "https://api.github.com/users/" + un + "?access_token=" + tk;
 		StartCoroutine(GetJSON(url_attribute));
 	}	
 
@@ -74,18 +74,22 @@ public class LoginManager : MonoBehaviour {
 			//logs json from Github
 			Debug.Log(www.downloadHandler.text);
 			JsonUtility.FromJsonOverwrite(www.downloadHandler.text, this);
-			string total_private_repos = (this.total_private_repos);
-			Debug.Log(total_private_repos);
+            if (!www.downloadHandler.text.Contains("total_private_repos"))
+            {
+                Debug.Log("Invalid Access Token for user " + this.username);
+            } else
+            {
+                //gets avatar_url and grabs image from web
+                string image_url = (this.avatar_url);
+                var www_image = new WWW(image_url);
+                yield return www_image; // waits until image is downloaded
 
-			//gets avatar_url and grabs image from web
-			string image_url = (this.avatar_url);
-			var www_image = new WWW(image_url);
-			yield return www_image; // waits until image is downloaded
-			//display image as a UI texture
-			Texture2D texture = new Texture2D(1, 1);
-			www_image.LoadImageIntoTexture(texture);
-			Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one / 2);
-			GetComponent<SpriteRenderer>().sprite = sprite;
+                //display image as a UI texture
+                Texture2D texture = new Texture2D(1, 1);
+                www_image.LoadImageIntoTexture(texture);
+                Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one / 2);
+                GetComponent<SpriteRenderer>().sprite = sprite;
+            }
 		}
 	}
 }
