e43531b9ec35a69ad5ccf383f033f4341f572d4a
Ryan Feldman <ryanjfeldman@gmail.com>
2017-07-10 14:25:32 -0400

    Added a webpage script that opens links given a url.

---
+++ Assets/Scripts/Webpage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Webpage : MonoBehaviour {
+
+	public string _url = "";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Open ()
+	{
+		Debug.Log ("Opening webpage... " + _url);
+		Application.OpenURL(_url);
+	}
+}
